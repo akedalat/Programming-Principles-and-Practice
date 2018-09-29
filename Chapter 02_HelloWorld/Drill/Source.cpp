42b97c034b5566b9f52253a5bc4af6e029064069
Kian <akedalat@gmail.com>
2018-09-29 18:16:15 +0100

    Made new project and some changes to codes

---
+++ Chapter 02_HelloWorld/Drill/Source.cpp
@@ -0,0 +1,14 @@
+
+#include "std_lib_facilities.h"
+
+
+int main()  {       // C++ programs start by executing the function main
+	
+	std::cout	<< "Hello, programming!\n"   // output “Hello, World!”
+				<< "here we go!\n"; 
+		keep_window_open();           // wait for a character to be entered
+
+	return 0;
+}
+
+
---
+++ Chapter 03_Objects_Types_Values/Detect_Odd_Even/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main (){
+
+	std::cout<< "Please enter an integer value" << std::endl;
+
+int val {0};
+
+	while (std::cin>> val) {
+
+		
+
+		if (val % 2 == 0) {
+
+		std::cout<< "The value " << val << " is an even number" << std::endl;
+		}
+		else {  
+
+		std::cout<< "The value " << val << " is an odd number" << std::endl;
+		}
+		
+}
+	return 0;
+}
